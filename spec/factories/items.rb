f5b990ff28536bf9240fa5542033958842a84991
yu-ta2401 <sid.melodyranbu.cuoria@gmail.com>
2020-08-25 21:26:53 +0900

    テストコード実装

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,14 @@
 FactoryBot.define do
   factory :item do
-    
+    name {'あああ'}
+    price {'500'}
+    explanation {'ああああ'}
+    category_id {3}
+    state_id {3}
+    burden_id {3}
+    shippingorigin_id {3}
+    day_id {3}
+    user_id {2}
+    association :user
   end
 end
