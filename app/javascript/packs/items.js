e6b0a54934ffa41abde47982b426ee10a81abd88
yu-ta2401 <sid.melodyranbu.cuoria@gmail.com>
2020-08-26 14:53:33 +0900

    行削除

--- app/javascript/packs/items.js
+++ app/javascript/packs/items.js
@@ -1,7 +1,6 @@
 function price() {
   const tagInput = document.getElementById('item-price');
-  
-  
+
   tagInput.addEventListener("keyup", function(){
       const str = this.value;
     
@@ -10,14 +9,7 @@ function price() {
       tax_price.innerHTML = Math.round(tax);
       const profit_calc = str - tax;
       const profit_price = document.getElementById('profit');
-      profit_price.innerHTML = profit_calc;
-      
-      
-      
-      
-    
-    
-    
+      profit_price.innerHTML = profit_calc; 
   });
 }
 window.addEventListener("load", price);
