d5c6a942d76cb9413266dcf3a24ef662897ff086
yu-ta2401 <sid.melodyranbu.cuoria@gmail.com>
2020-08-26 14:29:34 +0900

    コメントアウト削除

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,10 +1,1 @@
-# class Article < ApplicationRecord
-#   extend ActiveHash::Associations::ActiveRecordExtensions
-#   belongs_to_active_hash :category
 
-#   #空の投稿を保存できないようにする
-#   validates :title, :text, :category, presence: true
-
-#   #ジャンルの選択が「--」の時は保存できないようにする
-#   validates :category_id, numericality: { other_than: 1 } 
-# end
