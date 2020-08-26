f32ffe900416414334dad173fe3ef9b13d64ae0f
yu-ta2401 <sid.melodyranbu.cuoria@gmail.com>
2020-08-26 14:30:00 +0900

    出品機能の修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -6,7 +6,6 @@ class ItemsController < ApplicationController
 
   def index
     query = "SELECT * FROM items"
-    @items = Item.find_by_sql(query)
     @items = Item.order("created_at DESC")
   end
 
