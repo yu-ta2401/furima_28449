51c71ace8cf3fefc830319e1a093547afe30aeb5
yu-ta2401 <sid.melodyranbu.cuoria@gmail.com>
2020-08-31 22:31:58 +0900

    データベースの修正

---
+++ db/migrate/20200826023946_create_addresses.rb
@@ -0,0 +1,15 @@
+class CreateAddresses < ActiveRecord::Migration[6.0]
+  def change
+    create_table :addresses do |t|
+      t.string     :postalcode    ,default: ""  ,null: false
+      t.integer    :prefecture_id                ,null: false
+      t.string     :city          ,default: ""  ,null: false
+      t.string     :address       ,default: ""  ,null: false
+      t.string     :place         ,default: ""
+      t.string     :phonenumber                        ,null: false
+      t.references :user                     ,null: false, foreign_key: true
+      t.references :item                     ,null: false, foreign_key: true
+      t.timestamps
+    end
+  end
+end
