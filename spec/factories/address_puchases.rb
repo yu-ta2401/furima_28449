e0e684587c409de40929ef68353e258407b6acc5
yu-ta2401 <sid.melodyranbu.cuoria@gmail.com>
2020-09-01 19:31:04 +0900

    テストコード修正

--- spec/factories/address_puchases.rb
+++ spec/factories/address_puchases.rb
@@ -1,10 +1,10 @@
 FactoryBot.define do
   factory :address_purchase do
-    postal_code    {"123-4567"}
+    postalcode    {"123-4567"}
     prefecture_id  {8}
     city           {"東京"}
     address        {"1-1"}
-    tell           {"09089899009"}
+    phonenumber           {"09089899009"}
     token          {"tok_12as34cv"}
   end
 end
--- spec/models/address_purchases_spec.rb
+++ spec/models/address_purchases_spec.rb
@@ -21,14 +21,14 @@ RSpec.describe "AddressPurchases", type: :model do
         expect(@address_purchase.errors.full_messages).to include("Token can't be blank")
       end
       it "郵便番号がないと購入できない" do
-        @address_purchase.postal_code = nil
+        @address_purchase.postalcode = nil
         @address_purchase.valid?
-        expect(@address_purchase.errors.full_messages).to include("Postal code can't be blank")
+        expect(@address_purchase.errors.full_messages).to include("Postalcode can't be blank")
       end
       it "郵便番号にハイフン（-）がないと購入できない" do
-        @address_purchase.postal_code = "1234567"
+        @address_purchase.postalcode = "1234567"
         @address_purchase.valid?
-        expect(@address_purchase.errors.full_messages).to include("Postal code is invalid")
+        expect(@address_purchase.errors.full_messages).to include("Postalcode is invalid")
       end
       it "都道府県の情報がないと購入できない" do
         @address_purchase.prefecture_id = ""
@@ -46,14 +46,14 @@ RSpec.describe "AddressPurchases", type: :model do
         expect(@address_purchase.errors.full_messages).to include("Address can't be blank")
       end
       it "電話番号がないと購入できない" do
-        @address_purchase.tell = ""
+        @address_purchase.phonenumber= ""
         @address_purchase.valid?
-        expect(@address_purchase.errors.full_messages).to include("Tell can't be blank")
+        expect(@address_purchase.errors.full_messages).to include("Phonenumber can't be blank")
       end
-      it "電話番号にハイフン（-）があるとと購入できない" do
-        @address_purchase.tell = "090-6789-1234"
+      it "電話番号が11桁以内じゃないと購入できない" do
+        @address_purchase.phonenumber = "09067891234"
         @address_purchase.valid?
-        expect(@address_purchase.errors.full_messages).to include("Tell is invalid")
+        expect(@address_purchase.errors.full_messages).to include
       end
     end
   end
