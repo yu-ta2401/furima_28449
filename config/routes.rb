6997636ad50ffdc05040a6365fe185843763f4be
yu-ta2401 <sid.melodyranbu.cuoria@gmail.com>
2020-09-01 19:31:17 +0900

    コメントアウト削除

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users
-  # get 'home/index'
-  # get 'home/show'
-  # get 'home/new'
   root to: 'items#index'
   resources :articles
   resources :users, only: [:edit, :update]
